e502c41db9457660a41b4fc8e04d47e5a4c86cd4
William Le <williamtrungle@gmail.com>
2021-10-07 01:07:57 -0400

    feat(app): convert uploaded files to in memory files

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
-
-with open("README.md", "r") as readme:
-    title = readme.readline().strip('#').strip()
-    description = readme.read()
+from memory_tempfile import MemoryTempfile
 
 
 if __name__ == '__main__':
-    st.set_page_config(page_title="FCD")
-    st.title(title)
-    st.markdown(description)
+    st.set_page_config(page_title="Raman Spectroscopy")
+
+    # Sidebar
+    st.sidebar.header("Renishaw Acquisition")
+    for file in st.sidebar.file_uploader("Spectral data", ["wdf"], True):
+        with MemoryTempfile().TemporaryFile() as out:
+            out.write(file.read())
+
+    # Main
+    with open("README.md", "r") as readme:
+        st.title(readme.readline().strip('#').strip())
+        st.markdown(readme.read())
