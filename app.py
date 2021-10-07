82a1229b3a2d7e6b1b747f3e482571f7b4363581
William Le <williamtrungle@gmail.com>
2021-10-07 01:41:19 -0400

    style: refactor long lines into variables

--- app.py
+++ app.py
@@ -9,10 +9,8 @@ if __name__ == '__main__':
 
     # Sidebar
     st.sidebar.header("Renishaw Acquisition")
-    df = pd.DataFrame({
-        file.name: parse(file)
-        for file in st.sidebar.file_uploader("Spectral data", ["wdf"], True)
-    })
+    files = st.sidebar.file_uploader("Spectral data", ["wdf"], True)
+    df = pd.DataFrame({file.name: parse(file) for file in files})
     st.sidebar.metric("Acquisitions", f"{len(df.columns)} spectra")
 
     # Main
